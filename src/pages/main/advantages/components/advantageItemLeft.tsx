74495c7dc28943568027d3a5851d20dcfc7e6113
Zahar Kudriavcev <zaharkud162@gmail.com>
2023-10-25 21:12:36 +0300

    Main page advantages section adaptive changes

--- src/pages/main/advantages/components/advantageItemLeft.tsx
+++ src/pages/main/advantages/components/advantageItemLeft.tsx
@@ -8,12 +8,12 @@ interface IAdvantageItemLeft {
 
 const AdvantageItemLeft = ({ title, description, img }: IAdvantageItemLeft) => {
   return (
-    <div className={style.content}>
-      <div className={style.descr}>
+    <div className={style.content} style={{ columnGap: "30px" }}>
+      <div className={style.descr} style={{ order: 2 }}>
         <h3 className={style.title_h3}>{title}</h3>
         <p className={style.text}>{description}</p>
       </div>
-      <div className={style.graphics}>
+      <div className={style.graphics} style={{ order: 1 }}>
         <img src={img} alt="" style={{ left: "-15px" }} />
       </div>
     </div>
