9e881e8da620f3b5ffd69c3834793d38f6eaf183
Zahar Kudriavcev <zaharkud162@gmail.com>
2023-10-25 20:49:28 +0300

    Main page mobile version changes

--- src/components/serviceCard/serviceCard.tsx
+++ src/components/serviceCard/serviceCard.tsx
@@ -1,17 +1,20 @@
 import style from "./serviceCard.module.scss";
-import img from "assets/icons/services/cam.svg";
 
-const ServiceCard: React.FC = () => {
+interface IServiceCard {
+  title: string;
+  text: string;
+  img: any;
+}
+
+const ServiceCard = ({ title, text, img }: IServiceCard) => {
   return (
     <article className={style.content}>
       <div className={style.top}>
         <img src={img} />
       </div>
       <div className={style.bottom}>
-        <h4 className={style.title_h4}>Системы видеонаблюдения</h4>
-        <p className={style.text}>
-          Lorem ipsum dolor sit amet, conse ctetur adipiscing.
-        </p>
+        <h4 className={style.title_h4}>{title}</h4>
+        <p className={style.text}>{text}</p>
       </div>
     </article>
   );
--- src/pages/main/servicesSection/ServicesSection.tsx
+++ src/pages/main/servicesSection/ServicesSection.tsx
@@ -1,6 +1,11 @@
 import ServiceCard from "components/serviceCard/serviceCard";
 import style from "./servicesSection.module.scss";
 
+import cam from "assets/icons/services/cam.svg";
+import alarm from "assets/icons/services/alarm.svg";
+import fingerprint from "assets/icons/services/fingerprint.svg";
+import sound from "assets/icons/services/sound.svg";
+
 const ServicesSection: React.FC = () => {
   return (
     <div className={style.section}>
@@ -8,10 +13,26 @@ const ServicesSection: React.FC = () => {
         <div className={style.content}>
           <h2 className={style.title_h2}>Услуги</h2>
           <div className={style.cards}>
-            <ServiceCard></ServiceCard>
-            <ServiceCard></ServiceCard>
-            <ServiceCard></ServiceCard>
-            <ServiceCard></ServiceCard>
+            <ServiceCard
+              title="Системы видеонаблюдения"
+              text="Установка камер и вспомогательного оборудования"
+              img={cam}
+            ></ServiceCard>
+            <ServiceCard
+              title="Охранные системы"
+              text="Проектирование и монтирование охранной системы "
+              img={alarm}
+            ></ServiceCard>
+            <ServiceCard
+              title="Системы котроля и доступа (СКУД)"
+              text="Установка систем СКУД для преприятий"
+              img={fingerprint}
+            ></ServiceCard>
+            <ServiceCard
+              title="Потолочные акустические системы"
+              text="Установка звукового оборудования"
+              img={sound}
+            ></ServiceCard>
           </div>
         </div>
       </div>
