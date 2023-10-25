d7107a0f1fb8ffcd2efd9ac0b787b22a4cbe399c
Zahar Kudriavcev <zaharkud162@gmail.com>
2023-10-25 18:45:30 +0300

    Adapted promo block for mobile version

--- src/pages/main/promo/promo.tsx
+++ src/pages/main/promo/promo.tsx
@@ -6,6 +6,9 @@ const Promo: React.FC = () => {
       <div className={style.container}>
         <div className={style.content}>
           <h1 className={style.title}>Видео- наблюдение</h1>
+          <h1 className={`${style.title} ${style.titleMobile}`}>
+            Видео наблю дение
+          </h1>
           <p className={style.subtitle}>Продажа и установка на КМВ</p>
         </div>
       </div>
