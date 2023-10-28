2ca5c4bca885fad71497f9b7a8f351c722829132
Zahar Kudriavcev <zaharkud162@gmail.com>
2023-10-28 15:26:53 +0300

    Trying to make scrollable document container

--- src/pages/about/awards/Awards.tsx
+++ src/pages/about/awards/Awards.tsx
@@ -1,8 +1,17 @@
+import { useRef } from "react";
+
 import style from "./awards.module.scss";
 
 import mock from "assets/img/documents/mock.jpg";
+import mock2 from "assets/img/promo/promo.jpg";
+import FileItem from "components/fileItem/fileItem";
 
 const Awards: React.FC = () => {
+  //TODO: перенести рассчет длины на все элементы
+  //calculating max block`s witdh based on the inner image
+  const imgRef = useRef<HTMLImageElement>(null);
+  const filesItemMaxWidth = imgRef?.current?.clientWidth;
+
   return (
     <section className={style.section}>
       <div className={style.container}>
@@ -11,19 +20,10 @@ const Awards: React.FC = () => {
             <p>СЕРТИФИКАТЫ И НАГРАДЫ</p>
             <h2>И мы хороши в своем деле</h2>
           </div>
-          <div className={style.documentsList}>
-            <div className={style.documentsItem}>
-              <img src={mock} alt="document" />
-              <div className={style.bg}></div>
-            </div>
-            <div className={style.documentsItem}>
-              <img src={mock} alt="document" />
-              <div className={style.bg}></div>
-            </div>
-            <div className={style.documentsItem}>
-              <img src={mock} alt="document" />
-              <div className={style.bg}></div>
-            </div>
+          <div className={style.filesList}>
+            <FileItem img={mock}></FileItem>
+            <FileItem img={mock}></FileItem>
+            <FileItem img={mock2}></FileItem>
           </div>
         </div>
       </div>
--- src/pages/about/documents/Documents.tsx
+++ src/pages/about/documents/Documents.tsx
@@ -1,8 +1,15 @@
+import { useRef } from "react";
+
 import style from "./documents.module.scss";
 
 import mock from "assets/img/documents/mock.jpg";
+import mock2 from "assets/img/promo/promo.jpg";
 
 const Documents: React.FC = () => {
+  //calculating max block`s witdh based on the inner image
+  const imgRef = useRef<HTMLImageElement>(null);
+  const filesItemMaxWidth = imgRef?.current?.clientWidth;
+
   return (
     <section className={style.section}>
       <div className={style.container}>
@@ -11,18 +18,18 @@ const Documents: React.FC = () => {
             <p>ДОКУМЕНТЫ</p>
             <h2>У нас есть все необходимые разрешения</h2>
           </div>
-          <div className={style.documentsList}>
-            <div className={style.documentsItem}>
+          <div className={style.filesList}>
+            <div className={style.filesItem}>
               <img src={mock} alt="document" />
-              <div className={style.bg}></div>
             </div>
-            <div className={style.documentsItem}>
-              <img src={mock} alt="document" />
-              <div className={style.bg}></div>
+            <div className={style.filesItem}>
+              <img src={mock2} alt="document" />
             </div>
-            <div className={style.documentsItem}>
-              <img src={mock} alt="document" />
-              <div className={style.bg}></div>
+            <div
+              className={style.filesItem}
+              style={{ maxWidth: filesItemMaxWidth }}
+            >
+              <img ref={imgRef} src={mock} alt="document" />
             </div>
           </div>
         </div>
