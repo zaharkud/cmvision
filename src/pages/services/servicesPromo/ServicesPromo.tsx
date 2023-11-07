897b03db3dd7686be2fbf15f3e8f6381adaf9cdc
Zahar Kudriavcev <zaharkud162@gmail.com>
2023-11-07 16:56:25 +0300

    Added description for services

--- src/pages/services/servicesPromo/ServicesPromo.tsx
+++ src/pages/services/servicesPromo/ServicesPromo.tsx
@@ -64,8 +64,16 @@ const camSystems = () => {
   return (
     <div className={style.text}>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
-        dolor sit amet, consectetur adipiscing elit.
+        Сейчас системы видеонаблюдения устанавливаются не только на таких
+        крупных коммерческих объектах, как банки, заводы, предприятия, но и в
+        небольших офисах, жилых домах, квартирах.
+      </p>
+      <p>
+        Система видеонаблюдения позволит отслеживать происходящее на охраняемом
+        объекте в режиме реального времени, играя важную роль в обеспечении
+        безопасности. К этому вопросу необходимо подходить предельно серьезно.
+        Настройка камер и монтаж камер видеонаблюдения лучше доверить опытным
+        специалистам.
       </p>
     </div>
   );
@@ -75,12 +83,15 @@ const securitySystems = () => {
   return (
     <div className={style.text}>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
-        dolor
+        Охранные системы – это современный способ обеспечения безопасности на
+        объекте, включая такие угрозы, как пожар или утечка воды.
       </p>
       <p>
-        g elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem
-        ipsum dolor sit amet, consectetur adipiscing elit.
+        Безусловно, можно установить только систему видеонаблюдения и стараться
+        постоянно контролировать ситуацию на объекте. Но лучшим вариантом
+        является тандем охранной системы и видеонаблюдения в доме, в офисе, на
+        складе или в магазине. Это позволит не только разобраться в деталях уже
+        произошедшего, но и предотвращать неприятные случаи.
       </p>
     </div>
   );
@@ -90,10 +101,18 @@ const pacsSystems = () => {
   return (
     <div className={style.text}>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
-        dolor sit amet, consectetur
+        Большинство современных предприятий, офисов, складских комплексов и
+        прочих объектов не обходятся без системы контроля и управления доступом
+        (СКУД).
       </p>
-      <p>g elit. Lorem ipsum dolor sit amet,</p>
+      <p>
+        Установка системы контроля и управления доступом (СКУД) не только
+        обеспечивает безопасность вашему бизнесу и сотрудникам, но и позволяет
+        решать такие задачи, как: учет рабочего времени, предоставление
+        выборочного доступа в помещения конкретным сотрудникам, организация
+        пропускной системы, оперативное блокирование или разблокирование дверей,
+        идентификация личности и прочие.
+      </p>
     </div>
   );
 };
@@ -102,10 +121,10 @@ const soundSystems = () => {
   return (
     <div className={style.text}>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
-        dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit.
+        Различные типы акустических систем позволят решить такие задачи, как:
+        оповещение об опасной ситуации, транслирование объявление и рекламных
+        сообщений, воспроизведение фоновой музыки и организация двухсторонней
+        аудиосвязи.
       </p>
     </div>
   );
