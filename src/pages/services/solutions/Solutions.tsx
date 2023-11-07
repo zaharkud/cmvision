b0469f54995e1ce1a98369deaf8d49ccfd5fc9c7
Zahar Kudriavcev <zaharkud162@gmail.com>
2023-11-07 22:47:27 +0300

    Added description for solutions

--- src/pages/services/solutions/Solutions.tsx
+++ src/pages/services/solutions/Solutions.tsx
@@ -29,13 +29,31 @@ const Solutions: React.FC = () => {
                   className={style.btn}
                   onClick={() => setSolutionDescription(factorySolution)}
                 >
-                  Завод
+                  Производство
                 </li>
                 <li
                   className={style.btn}
-                  onClick={() => setSolutionDescription(bankSolution)}
+                  onClick={() => setSolutionDescription(warehouseSolution)}
                 >
-                  Банк
+                  Склад
+                </li>
+                <li
+                  className={style.btn}
+                  onClick={() => setSolutionDescription(restaurantSolution)}
+                >
+                  Ресторан
+                </li>
+                <li
+                  className={style.btn}
+                  onClick={() => setSolutionDescription(officeSolution)}
+                >
+                  Офис
+                </li>
+                <li
+                  className={style.btn}
+                  onClick={() => setSolutionDescription(houseSolution)}
+                >
+                  Частный дом
                 </li>
               </ul>
             </div>
@@ -104,27 +122,34 @@ const factorySolution = () => {
         <div className={style.cardInfo}>
           <div className={style.cardLeft}>
             <div className={style.descr}>
-              <h3 className={style.title_h3}>Завод</h3>
+              <h3 className={style.title_h3}>Производство</h3>
               <p className={style.text}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                sagittis mauris ac enim sagittis dignissim.
+                Установка системы видеонаблюдения на производстве. Используются
+                уличные камеры видеонаблюдения для контроля за периметром.
+                Настройка удаленного доступа на видеорегистраторе в подарок.
               </p>
             </div>
           </div>
           <div className={style.cardRight}>
             <p className={style.purchaseLine}>
-              Камера 15 шт. по
-              <br /> 2 500 РУБ
+              Монтаж кабеля, 150м. по 16р.
+              <br /> 2400 РУБ
             </p>
             <p className={style.purchaseLine}>
-              Ноутбук 2 шт. по <br />
-              25 000 РУБ
+              Установка камер видеонаблюдения 4шт. по 850р.
+              <br />
+              3400 РУБ
             </p>
             <p className={style.purchaseLine}>
-              Провод 35 м. по <br />
-              300 РУБ
+              Подключение блока питания 1шт. по 350р.
+              <br />
+              350 РУБ
             </p>
-            <p className={style.finalCost}>70 000 РУБ</p>
+            <p className={style.purchaseLine}>
+              Установка и настройка видеорегистратора <br />
+              1500 РУБ
+            </p>
+            <p className={style.finalCost}>7 650 РУБ</p>
           </div>
         </div>
       </div>
@@ -132,7 +157,7 @@ const factorySolution = () => {
   );
 };
 
-const bankSolution = () => {
+const warehouseSolution = () => {
   return (
     <div className={style.solutionsCard}>
       <div className={style.cardContent}>
@@ -144,25 +169,181 @@ const bankSolution = () => {
             <div className={style.descr}>
               <h3 className={style.title_h3}>Банк</h3>
               <p className={style.text}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                sagittis mauris ac enim sagittis dignissim.
+                Установка системы видеонаблюдения в складском помещении.
+                Используются уличные камеры видеонаблюдения для контроля за
+                периметром. Настройка удаленного доступа на видеорегистраторе в
+                подарок.
               </p>
             </div>
           </div>
           <div className={style.cardRight}>
             <p className={style.purchaseLine}>
-              Камера 15 шт. по
-              <br /> 2 500 РУБ
+              Монтаж кабеля, 200м. по 16р.
+              <br /> 3200 РУБ
             </p>
             <p className={style.purchaseLine}>
-              Ноутбук 2 шт. по <br />
-              25 000 РУБ
+              Установка камер видеонаблюдения 8шт. по 850р.
+              <br />
+              6800 РУБ
             </p>
             <p className={style.purchaseLine}>
-              Провод 35 м. по <br />
-              300 РУБ
+              Подключение блока питания 1шт. по 350р.
+              <br />
+              350 РУБ
             </p>
-            <p className={style.finalCost}>90 000 РУБ</p>
+            <p className={style.purchaseLine}>
+              Установка и настройка видеорегистратора <br />
+              1500 РУБ
+            </p>
+            <p className={style.finalCost}>11 850 РУБ</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const restaurantSolution = () => {
+  return (
+    <div className={style.solutionsCard}>
+      <div className={style.cardContent}>
+        <div className={style.cardImg}>
+          <img src={mock} alt="gazprom"></img>
+        </div>
+        <div className={style.cardInfo}>
+          <div className={style.cardLeft}>
+            <div className={style.descr}>
+              <h3 className={style.title_h3}>Банк</h3>
+              <p className={style.text}>
+                Установка системы видеонаблюдения в ресторане. Используются
+                купольные камеры внутреннего типа. Настройка удаленного доступа
+                на видеорегистраторе в подарок.
+              </p>
+            </div>
+          </div>
+          <div className={style.cardRight}>
+            <p className={style.purchaseLine}>
+              Монтаж кабеля, 100м. по 16р.
+              <br /> 1600 РУБ
+            </p>
+            <p className={style.purchaseLine}>
+              Монтаж кабель-канала, 100м. по 35р.
+              <br /> 3500 РУБ
+            </p>
+            <p className={style.purchaseLine}>
+              Установка камер видеонаблюдения 8шт. по 550р.
+              <br />
+              4400 РУБ
+            </p>
+            <p className={style.purchaseLine}>
+              Подключение блока питания 1шт. по 350р.
+              <br />
+              350 РУБ
+            </p>
+            <p className={style.purchaseLine}>
+              Установка и настройка видеорегистратора <br />
+              1500 РУБ
+            </p>
+            <p className={style.finalCost}>11 350 РУБ</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const officeSolution = () => {
+  return (
+    <div className={style.solutionsCard}>
+      <div className={style.cardContent}>
+        <div className={style.cardImg}>
+          <img src={mock} alt="gazprom"></img>
+        </div>
+        <div className={style.cardInfo}>
+          <div className={style.cardLeft}>
+            <div className={style.descr}>
+              <h3 className={style.title_h3}>Банк</h3>
+              <p className={style.text}>
+                Установка системы видеонаблюдения в помещениях офиса.
+                Используются купольные камеры внутреннего типа. Настройка
+                удаленного доступа на видеорегистраторе в подарок.
+              </p>
+            </div>
+          </div>
+          <div className={style.cardRight}>
+            <p className={style.purchaseLine}>
+              Монтаж кабеля, 160м. по 16р.
+              <br /> 2560 РУБ
+            </p>
+            <p className={style.purchaseLine}>
+              Монтаж кабель-канала, 160м. по 35р.
+              <br /> 5600 РУБ
+            </p>
+            <p className={style.purchaseLine}>
+              Установка камер видеонаблюдения 8шт. по 550р.
+              <br />
+              4400 РУБ
+            </p>
+            <p className={style.purchaseLine}>
+              Подключение блока питания 1шт. по 350р.
+              <br />
+              350 РУБ
+            </p>
+            <p className={style.purchaseLine}>
+              Установка и настройка видеорегистратора <br />
+              1500 РУБ
+            </p>
+            <p className={style.finalCost}>16 900 РУБ</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const houseSolution = () => {
+  return (
+    <div className={style.solutionsCard}>
+      <div className={style.cardContent}>
+        <div className={style.cardImg}>
+          <img src={mock} alt="gazprom"></img>
+        </div>
+        <div className={style.cardInfo}>
+          <div className={style.cardLeft}>
+            <div className={style.descr}>
+              <h3 className={style.title_h3}>Банк</h3>
+              <p className={style.text}>
+                Установка системы видеонаблюдения в на территории частного дома.
+                Используются уличные камеры видеонаблюдения для контроля за
+                периметром. Настройка удаленного доступа на видеорегистраторе в
+                подарок.
+              </p>
+            </div>
+          </div>
+          <div className={style.cardRight}>
+            <p className={style.purchaseLine}>
+              Монтаж кабеля в гофротрубе, 40м. по 19р.
+              <br /> 760 РУБ
+            </p>
+            <p className={style.purchaseLine}>
+              Монтаж гофротрубы, 40м. по 17р.
+              <br /> 680 РУБ
+            </p>
+            <p className={style.purchaseLine}>
+              Установка камер видеонаблюдения 4шт. по 850р.
+              <br />
+              3400 РУБ
+            </p>
+            <p className={style.purchaseLine}>
+              Подключение блока питания 1шт. по 350р.
+              <br />
+              350 РУБ
+            </p>
+            <p className={style.purchaseLine}>
+              Установка и настройка видеорегистратора <br />
+              1500 РУБ
+            </p>
+            <p className={style.finalCost}>6 690 РУБ</p>
           </div>
         </div>
       </div>
